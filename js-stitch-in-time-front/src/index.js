a683f3cecf19f3639299c2ee31ebe832d14b41c2
1-8192 <allegranzia@gmail.com>
2019-04-15 18:54:37 -0400

    Got a click event to work with p5

--- js-stitch-in-time-front/src/index.js
+++ js-stitch-in-time-front/src/index.js
@@ -1,25 +1,33 @@
+function preload() {
+  // loadFont('fonts/vd.ttf');
+}
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  cnv = createCanvas(window.innerWidth, window.innerHeight);
   img = loadImage('./images/cover.png');
   textSize(36);
-  textFont("Courier New");
+  textFont('./fonts/vd.ttf');
+  textStyle('BOLD');
+  y = height;
+  nextButton = createButton('click here');
+  nextButton.mousePressed(changeBg);
+
 }
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
-  background(img, 0, 0, width, height);
-
+  bg = background(img, 0, 0, width, height);
 
   //Welcome message
-  let x= text("Hi there. Welcome to A Stitch in Time, a digital storybook based on the book by Himradi Das and Veena Prasad. Illustrations by Ankitha Kini", 200, 200, 800, 300);
-
-  function mousePressed() {
-    if (x) {
-      x = text("What's your name?", 200, 200, 800, 300);
-    }
+  if (y > 200) {
+    y = y - 1
   }
+  fill(59, 70, 73);
+  intro = text("Hi there. Welcome to A Stitch in Time, a digital storybook based on the book by Himradi Das and Veena Prasad. Illustrations by Ankitha Kini", 200, y, 800, 300);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-
-});
+function changeBg() {
+  img = loadImage('./images/a-stitch-in-time_2.png')
+  bg = background(img, 0, 0, width, height);
+  intro = text("")
+}
