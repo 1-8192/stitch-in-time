d2311813509b0d45dc16123782a42c75135058ca
1-8192 <allegranzia@gmail.com>
2019-04-16 10:20:26 -0400

    Working on welcome page

--- js-stitch-in-time-front/src/index.js
+++ js-stitch-in-time-front/src/index.js
@@ -1,3 +1,8 @@
+let userName;
+let pageCount = 2;
+let welcomeButton;
+let signIn;
+
 function preload() {
   // loadFont('fonts/vd.ttf');
 }
@@ -8,9 +13,17 @@ function setup() {
   textSize(36);
   textFont('./fonts/vd.ttf');
   textStyle('BOLD');
+  //height for text scrolling
   y = height;
-  nextButton = createButton('click here');
-  nextButton.mousePressed(changeBg);
+  //button to click next
+  // welcomeButton = createButton('Start reading');
+  // welcomeButton.position(600,300);
+  // welcomeButton.mousePressed(changeBg);
+  // //input button for user name
+  //
+  // let signIn = createInput("Hi, what's your name?");
+  // signIn.position(450, 300)
+  // signIn.input(myInputEvent);
 
 }
 
@@ -19,15 +32,34 @@ function draw() {
   bg = background(img, 0, 0, width, height);
 
   //Welcome message
-  if (y > 200) {
-    y = y - 1
+  if (y > 150) {
+    y = y - 5
   }
   fill(59, 70, 73);
+  if (pageCount === 2) {
   intro = text("Hi there. Welcome to A Stitch in Time, a digital storybook based on the book by Himradi Das and Veena Prasad. Illustrations by Ankitha Kini", 200, y, 800, 300);
+};
+
+  if (pageCount === 2) {
+    welcomeButton = createButton('Start reading');
+    welcomeButton.position(600,300);
+    welcomeButton.mouseClicked(changeBg(pageCount));
+    //input button for user name
+
+    let signIn = createInput("Hi, what's your name?");
+    signIn.position(450, 300)
+    signIn.input(myInputEvent);
+  }
 }
 
-function changeBg() {
-  img = loadImage('./images/a-stitch-in-time_2.png')
+//interactivity functions
+function myInputEvent() {
+  userName = this.value();
+};
+
+function changeBg(count) {
+  img = loadImage(`./images/a-stitch-in-time_${pageCount}.png`)
   bg = background(img, 0, 0, width, height);
-  intro = text("")
-}
+  removeElements();
+  pageCount ++;
+};
